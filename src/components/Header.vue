0d611be48e08883a70bdab0c082bd02a4008370f
prototowb <prototowb@gmail.com>
2022-01-19 11:08:42 +0100

    initial push

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <!-- Header -->
+  <header id="header" class="header__base">
+    <div class="header__container">
+      <!-- App Name -->
+      <a class="header__brand" href="#">Music</a>
+      <nav class="nav__base">
+        <!-- Primary Navigation -->
+        <ul class="nav__menu">
+          <!-- Navigation Links -->
+          <li>
+            <a class="nav__link" href="#">Login / Register</a>
+          </li>
+          <li>
+            <a class="nav__link" href="#">Manage</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
